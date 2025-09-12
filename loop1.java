778b1090f856cb940ee06ea1b2466a5c2be00477
TwillightSniper <twillightsniper257@gmail.com>
2025-09-12 18:09:53 +0530

    change

--- loop1.java
+++ loop1.java
@@ -3,7 +3,7 @@ class loop1
     public static void main(String args[])
     {
         int i;
-        for(i=0;i<20;i++)
+        for(i=0;i<25;i++)
         {
             System.out.println(i);
         }
