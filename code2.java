c9903e99dbe938f7caec6f52926d2c0fae39eb33
TwillightSniper <twillightsniper257@gmail.com>
2025-09-12 17:00:01 +0530

    12 Sep

---
+++ code2.java
@@ -0,0 +1,7 @@
+class code2
+{
+   public static void main(String args[])
+   {
+       System.out.println("hi");
+   }
+}
---
+++ loop1.java
@@ -0,0 +1,11 @@
+class loop1
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0;i<20;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
